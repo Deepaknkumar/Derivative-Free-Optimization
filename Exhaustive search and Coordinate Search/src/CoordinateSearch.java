c3f05fa1ffea61cec7b0a870843d21adb330c701
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-01-23 16:06:04 -0800

    Coordinate Search class
    Template for Coordinate Search class

---
+++ Exhaustive search and Coordinate Search/src/CoordinateSearch.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Deepak Kumar on 23/01/2017.
+ */
+public class CoordinateSearch {
+    public int numiter = 20;
+    public Point2D initPoint = new Point2D();
+
+}
---
+++ Exhaustive search and Coordinate Search/src/Point2D.java
@@ -0,0 +1,19 @@
+
+
+/**
+ * Created by Deepak Kumar on 23/01/2017.
+ */
+public class Point2D {
+    public double x;
+    public double y;
+
+    public Point2D(){
+        this.x = 0.0;
+        this.y = 0.0;
+    }
+
+    public Point2D(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+}
